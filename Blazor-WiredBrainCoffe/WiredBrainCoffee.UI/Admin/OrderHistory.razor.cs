0689a8821450cec06e829a132fe4da9acd1074f7
Zeynel Şahin <50925966+zeynelsahin@users.noreply.github.com>
2023-01-14 00:59:29 +0300

    Blazor QuickGrid

--- Blazor-WiredBrainCoffe/WiredBrainCoffee.UI/Admin/OrderHistory.razor.cs
+++ Blazor-WiredBrainCoffe/WiredBrainCoffee.UI/Admin/OrderHistory.razor.cs
@@ -10,11 +10,17 @@ namespace WiredBrainCoffee.UI.Admin
         [Inject]
         public IOrderService OrderService { get; set; }
 
-        List<Order> items = new List<Order>();
+        IQueryable<Order> items = new List<Order>().AsQueryable();
+
+        PaginationState pagination = new PaginationState() { ItemsPerPage = 4 };
+
+        private string nameFilter = string.Empty;
+
+        private IQueryable<Order> FilteredItems => items.Where(p => p.LastName.Contains(nameFilter, StringComparison.CurrentCultureIgnoreCase));
 
         protected override async Task OnInitializedAsync()
         {
-            items = await OrderService.GetOrders();
+            items = (await OrderService.GetOrders()).AsQueryable();
         }
     }
 }
